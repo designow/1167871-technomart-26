7b7a90e2091e1b6d28179d8d5c202c960ea74186
designow <alex.designow@yandex.ru>
2019-10-12 20:28:32 +0700

    Info-popup commit

---
+++ js/catalog.js
@@ -0,0 +1,25 @@
+        let modalclose = document.querySelector(".modal-close");
+        let btncontinue = document.querySelector(".continue");
+
+          //Закрытие формы добавления в корзину
+          modalclose.addEventListener("click", function (evt) {
+               modalinfo.classList.remove("modal-show");
+          });
+
+          //Продолжить покупки формы добавления в корзину
+          btncontinue.addEventListener("click", function (evt) {
+               modalinfo.classList.remove("modal-show");
+          });
+
+
+        
+        //Открытие формы добавления в корзину
+        let modalinfo = document.querySelector(".modal-info"),
+        buy = Array.prototype.slice.call(document.querySelectorAll(".buy")),
+        onClickButton = function(evt){
+          evt.preventDefault(),
+        document.querySelector(".modal-info").classList.add("modal-show")};
+        
+        buy.forEach(function(evt){
+          evt.addEventListener("click", onClickButton)
+        });
